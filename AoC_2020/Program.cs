f4082e14f2ad12dcaf4d2bd040d248fbfb2d7753
Paul Ogle <ogleuwyo@gmail.com>
2020-12-03 17:31:18 -0700

    AOC-Day-02-Problem-Two Added property to solve day 2 problem 2

--- AoC_2020/Program.cs
+++ AoC_2020/Program.cs
@@ -10,9 +10,14 @@ namespace AoC_2020
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            var dayTwoOutput = DayTwo.ValidPasswordCount(Properties.Resources.Exercise_02.Split(Environment.NewLine));
+            var dayTwoOutput = DayTwo.ValidPasswordCountProblemOne(Properties.Resources.Exercise_02.Split(Environment.NewLine));
             stopwatch.Stop();
             Console.WriteLine($"Day Two Output: {dayTwoOutput} completed in {stopwatch.Elapsed.TotalMilliseconds} milliseconds");
+            stopwatch.Reset();
+            stopwatch.Start();
+            var dayTwoOutputProblem02 = DayTwo.ValidPasswordCountProblemTwo(Properties.Resources.Exercise_02.Split(Environment.NewLine));
+            stopwatch.Stop();
+            Console.WriteLine($"Day Two Output: {dayTwoOutputProblem02} completed in {stopwatch.Elapsed.TotalMilliseconds} milliseconds");
         }
     }
 }
