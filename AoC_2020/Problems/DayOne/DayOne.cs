c173d10c8dbfc7c681bacdabdae8d86df1793a05
Paul Ogle <ogleuwyo@gmail.com>
2020-12-04 17:38:09 -0700

    AOC-Day-01

---
+++ AoC_2020/Problems/DayOne/DayOne.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC_2020.Problems.DayOne
+{
+
+   
+    public static class DayOne
+    {
+        public static long LargestMultipledValue { get; set; }
+        public static long MultipliedTwoValues(string[] lines)
+        {
+            LargestMultipledValue = 0;
+            
+            for(var i = 0; i<lines.Length; i++)
+            {
+                for(var j = i+1; j<lines.Length; j++)
+                {
+                    if(long.Parse(lines[j]) + long.Parse(lines[i]) == 2020)
+                    {
+                        long multipledValue = long.Parse(lines[j]) * long.Parse(lines[i]);
+                        if(multipledValue > LargestMultipledValue)
+                        {
+                            LargestMultipledValue = multipledValue;
+                        }
+                    }
+
+                }
+
+            }
+            return LargestMultipledValue;
+        }
+        public static long MultipliedThreeValues(string[] lines)
+        {
+            LargestMultipledValue = 0;
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                for (var j = i + 1; j < lines.Length; j++)
+                {
+                    for(var k = i+2; k<lines.Length; k++)
+                    {
+                        if (long.Parse(lines[j]) + long.Parse(lines[i]) + long.Parse(lines[k]) == 2020)
+                        {
+                            long multipledValue = long.Parse(lines[j]) * long.Parse(lines[i]) * long.Parse(lines[i]);
+                            if (multipledValue > LargestMultipledValue)
+                            {
+                                LargestMultipledValue = multipledValue;
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            return LargestMultipledValue;
+        }
+    }
+}
---
+++ AoC_2020/Problems/DayThree/DayThree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC_2020.Problems.DayThree
+{
+    public  class DayThree
+    {
+        public DayThree()
+        {
+            XPosition = 0;
+            YPosition = 0;
+        }
+        public int MapHeight { get; set; }
+        public int MapWidth { get; set; }
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public int TreesCounter { get; set; }
+        public int NextLineMovement { get; set; }
+        public long TreesEncountered(string[] treeLines)
+        {
+            MapWidth = treeLines[0].Length;
+            MapHeight = treeLines.Length;
+          
+            for(int i = 0; i < treeLines.Length; i++)
+            {
+                char[] treeLine = treeLines[i].ToCharArray();
+                var x = 5;
+            }
+
+            return 0;
+        }
+    }
+}
